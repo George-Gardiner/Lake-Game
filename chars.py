e2f0ed7aa0a2598d3c7ab8dade572b361bb0c1d3
SaiSauce <131722998+SaiSauce@users.noreply.github.com>
2023-08-10 11:02:29 -0400

    Update chars.py
    Sai's Collectible class(it is commented, but if needed you can use it)

--- chars.py
+++ chars.py
@@ -78,7 +78,7 @@ class animatedSprite(animation, sprite):
             super().update(frame)
 
 
-class collectable(animatedSprite):
+class collectable(animatedSprite): #sai
     def __init__(self, pos, img, ts, mfps, fun, fipn=None, size=None):
         self.exists = True
         if size is None:
@@ -104,9 +104,21 @@ class collectable(animatedSprite):
                     self.exists = False
             else:
                 self.col = False
-
-
-class interactable(collectable):
+'''           
+class Collectible: 
+    def __init__(self,x1,x2,y1,y2,x,y): 
+        self.shape = pygame.Rect(x1,x2,y1,y2)
+        self.shape.x = x
+        self.shape.y = y
+    def update(self):
+        self.shape.x = -100
+        self.shape.y = -100
+        c1 = Collectible(0, 0, 20, 20, random.randint(0, 640),random.randint(0, 480));
+        pygame.draw.rect(screen, (255, 0, 0), c1.shape)
+    if sprite.colliderect(c1.shape):
+        c1.update()
+'''    
+class interactable(collectable): #sai
     pass
 
 
